bdb403fda13b11f7bff2cbe1a959830b7a366c5a
parhatyadikar <38082603+parhatyadikar@users.noreply.github.com>
2018-07-28 16:46:23 -0400

    MMP--102 I just created java maven project on eclipse and push it to local repo.

---
+++ ferizeGithubServer/src/test/java/GithubServerTestFerize/FerizeGitserverClass.java
@@ -0,0 +1,9 @@
+package GithubServerTestFerize;
+
+public class FerizeGitserverClass {
+
+	public static void main(String[] args) {
+System.out.println("This is my test github testing class");
+	}
+
+}
