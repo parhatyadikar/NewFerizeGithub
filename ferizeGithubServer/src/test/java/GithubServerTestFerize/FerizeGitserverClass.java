8bf69d3b02a89d0836262b0870714f8468685579
parhatyadikar <38082603+parhatyadikar@users.noreply.github.com>
2018-07-28 16:54:48 -0400

    OMM 103 I just created local child branch and change code

--- ferizeGithubServer/src/test/java/GithubServerTestFerize/FerizeGitserverClass.java
+++ ferizeGithubServer/src/test/java/GithubServerTestFerize/FerizeGitserverClass.java
@@ -3,7 +3,8 @@ package GithubServerTestFerize;
 public class FerizeGitserverClass {
 
 	public static void main(String[] args) {
-System.out.println("This is my test github testing class");
+		System.out.println("This is my test github testing class");
+		System.out.println("I add the child branch on local repo");
 	}
 
 }
