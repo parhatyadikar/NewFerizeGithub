8b959aac8aae3797ca5cc626a802b7984fd7ba35
parhatyadikar <38082603+parhatyadikar@users.noreply.github.com>
2018-07-28 17:00:04 -0400

    OMM 104 I just created new branch on local repo as remote repo

--- ferizeGithubServer/src/test/java/GithubServerTestFerize/FerizeGitserverClass.java
+++ ferizeGithubServer/src/test/java/GithubServerTestFerize/FerizeGitserverClass.java
@@ -5,6 +5,8 @@ public class FerizeGitserverClass {
 	public static void main(String[] args) {
 		System.out.println("This is my test github testing class");
 		System.out.println("I add the child branch on local repo");
+		System.out.println("I add the child branch on local same as remote server");
+
 	}
 
 }
