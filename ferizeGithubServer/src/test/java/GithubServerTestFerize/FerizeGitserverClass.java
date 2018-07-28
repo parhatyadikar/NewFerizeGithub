c5fe8033d170f32ce4db66c3fd490b3a14f9b998
crazy <crazy@Anvar-PC.fios-router.home>
2018-07-28 19:54:29 -0400

    i don't know i am i doing

--- ferizeGithubServer/src/test/java/GithubServerTestFerize/FerizeGitserverClass.java
+++ ferizeGithubServer/src/test/java/GithubServerTestFerize/FerizeGitserverClass.java
@@ -3,16 +3,8 @@ package GithubServerTestFerize;
 public class FerizeGitserverClass {
 
 	public static void main(String[] args) {
-		System.out.println("This is my test github testing class");
-		System.out.println("I add the child branch on local repo");
-		System.out.println("I add the child branch on local same as remote server");
-		System.out.println("I am learning merge on local ");
-		// Anvar'
-		System.out.println("Hello world!!!");
-		System.out.println("Hello world!!!");
-		System.out.println("Hello world!!!");
-		System.out.println("Hello world!!!");
-		
+		System.out.println("108 version");
+	
 
 
 	}
